1554845983e0b483bc3a8d4d9445fb04a2bfc999
Vivian Yim <keroviv86@gmail.com>
2022-04-15 16:30:20 -0500

    finished day 3

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,5 +1,31 @@
 function fibonacci(num) {
-  // type your code here
+  // if(num<2){
+  //   return num
+  // }else{
+  //   return fibonacci(num-1)+fibonacci(num-2)
+  // }
+  // let arr=[]
+  // for(let i=0;i<=num;++i){
+  //   if(i<2){
+  //     arr.push(i)
+  //   }else{
+  //     arr.push(arr[i-2]+arr[i-1])
+  //   }
+  // }
+  // return arr[arr.length-1]
+  if (num < 2) return num
+
+  prev = 0
+  fib = 1
+
+  for (let i = 2; i <= num; ++i) {
+      next_fib = prev + fib
+      prev = fib
+      fib = next_fib
+  }
+  
+  return fib
+
 }
 
 if (require.main === module) {
